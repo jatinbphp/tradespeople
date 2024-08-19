4680a41c792dd26dd82391870ebf1602515b76c9
jatinbphp <jatin.p.php@gmail.com>
2024-08-19 10:28:46 +0530

    service detail page update

--- application/views/site/service_details.php
+++ application/views/site/service_details.php
@@ -288,27 +288,14 @@
 						<div class="col-sm-4">
 							<div class="sidebar-main" id="sidebar">
 								<div class="service-detail-sidebar">
-									<!-- <h2 class="title"><?php echo '£'.number_format($service_details['price'],2); ?></h2>
-									<p>15s Social Media Ad in 1 format of your choice: Landscape, Square or Vertical</p> -->
-
 									<div class="sidebar-icons">
-										<button class="menu" type="button" data-toggle="tooltip" data-placement="top" title="list">
-											<svg fill="#b5b6ba" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-												<path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-											</svg>
-										</button>
 										<div class="">
 											<button class="save" type="button" data-toggle="tooltip" data-placement="top" title="Save to list"><svg fill="#b5b6ba" width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M14.4469 1.95625C12.7344 0.496875 10.1875 0.759375 8.61561 2.38125L7.99999 3.01562L7.38436 2.38125C5.81561 0.759375 3.26561 0.496875 1.55311 1.95625C-0.409388 3.63125 -0.512513 6.6375 1.24374 8.45312L7.29061 14.6969C7.68124 15.1 8.31561 15.1 8.70624 14.6969L14.7531 8.45312C16.5125 6.6375 16.4094 3.63125 14.4469 1.95625Z"></path></svg></button>
 											<span class="collect-count"><?php echo $service_details['total_likes']; ?></span>
 										</div>
 										<button type="button" class="btn btn-outline" data-toggle="tooltip" data-placement="top" title="Share this service">
 											<svg width="16" height="16" viewBox="0 0 14 16" xmlns="http://www.w3.org/2000/svg" fill="currentFill"><path d="M11 10c-.707 0-1.356.244-1.868.653L5.929 8.651a3.017 3.017 0 0 0 0-1.302l3.203-2.002a3 3 0 1 0-1.06-1.696L4.867 5.653a3 3 0 1 0 0 4.694l3.203 2.002A3 3 0 1 0 11 10Z"></path></svg>
-										</button>
-										<button type="button" class="btn btn-outline" data-toggle="tooltip" data-placement="top" title="Share this service">
-											<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
-												<path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3"/>
-											</svg>
-										</button>
+										</button>										
 									</div>
 
 									<?php 
